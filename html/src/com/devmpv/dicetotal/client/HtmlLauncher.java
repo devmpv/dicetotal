28a76d1c7f69b495e07db92ecd5e3846ad72ede2
pmuravev <pmuravev@naumen.ru>
2024-01-01 15:34:27 +0300

    Initial commit

---
+++ html/src/com/devmpv/dicetotal/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.devmpv.dicetotal.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.devmpv.dicetotal.Dicetotal;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Dicetotal();
+        }
+}
